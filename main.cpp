d05f01bff7d6b002e6bce01278a3b77887964655
Mateusz Dołęga <md->
2021-05-25 09:49:28 +0200

    validation v3 - checking first and last element, repetition of operation signs

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 #include <validation.h>
 #include <basic.h>
 
+/* codeblocks
+#include "validation.h" 
+#include "basic.h"
+*/
+
 int main() {
     std::string expression;
     while (std::getline(std::cin, expression)){
         if(validate(expression)){
-            std::cout << parse(expression);
+            std::cout << parse(expression) << "\n";
         }
     }
     return 0;
--- validation.h
+++ validation.h
@@ -3,13 +3,37 @@
 
 #include <cctype>
 
-std::string charactersAllowed = "1234567890+-/*()[] "; // spaces allowed
-bool validate(std::string ex){
+std::string charactersAllowed = "1234567890+-/*()[]. "; // spaces allowed
+std::string aritmeticOperationsCharacters = "+-/*";
+
+bool validateNumbers(std::string ex){
     for (int a = 0; a<ex.size(); a++)
         if (charactersAllowed.find(ex[a]) == std::string::npos){
-            std::cout << "Wykryto zakazany symbol\n" << "na pozycji " << (a+1) << "\n";
+            std::cout << "Wykryto zakazany symbol na pozycji " << (a+1) << "\n";
             return false;
         }
+    return true;
+}
+
+bool validateStructure(std::string ex){
+    bool lastCharWasAOperationCharacter=false;
+    for (int a = 0; a<ex.size(); a++){
+        if ((aritmeticOperationsCharacters.find(ex[a]) != std::string::npos && (a == 0 || a == ex.size()-1)) || (aritmeticOperationsCharacters.find(ex[a]) != std::string::npos && lastCharWasAOperationCharacter)){// check if not allowed character is on 1 or last position and check if operation character is after other operation character
+            std::cout << "Zla struktura wyrazenia arytmetycznego\n";
+            return false;
+        }
+        lastCharWasAOperationCharacter = aritmeticOperationsCharacters.find(ex[a]) != std::string::npos;
+    }
+    return true;
+}
+
+bool validate(std::string ex){
+    if(!validateNumbers(ex))
+        return false;
+    if(!validateStructure(ex))
+        return false;
+
+
     return true;
 }
 #endif
